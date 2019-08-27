b72a10ab6b3fa7771c5810c233d4f5d864bf2428
echopecho <pbsmithtbn@gmail.com>
2019-08-27 16:44:27 -0500

    Complete stock_prices.py

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,28 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    current_buy = prices[0]
+    max_profit = 0
+    for price in prices:
+        current_buy = price if price < current_buy else current_buy
+        sell = price - current_buy if current_buy > 0 else 0
+        max_profit = sell if sell > max_profit else max_profit
+    return max_profit
 
 
-if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+if __name__ == "__main__":
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(description="Find max profit from prices.")
+    parser.add_argument(
+        "integers", metavar="N", type=int, nargs="+", help="an integer price"
+    )
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print(
+        "A profit of ${profit} can be made from the stock prices {prices}.".format(
+            profit=find_max_profit(args.integers), prices=args.integers
+        )
+    )
+
