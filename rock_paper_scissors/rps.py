eedc1895500d33f0fdd6ce8806fc1067ee9c7722
echopecho <pbsmithtbn@gmail.com>
2019-08-27 22:22:21 -0500

    Complete rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,26 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    outcomes = []
+    plays = ["rock", "paper", "scissors"]
+
+    def find_result(times, arr=[]):
+        if times == 0:
+            outcomes.append(arr)
+            return
+        for play in plays:
+            find_result(times - 1, arr + [play])
+
+    find_result(n, [])
+    return outcomes
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print("Usage: rps.py [num_plays]")
+
